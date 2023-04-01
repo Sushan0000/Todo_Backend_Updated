6ceb6f49e7cfa41f64a55ef6cabf287c25d32a12
Sushan <t2masu00@students.oamk.fi>
2023-04-02 01:37:49 +0300

    updated the connection details

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
 "use strict";
-var __awaiter =
-	(this && this.__awaiter) ||
-	function (thisArg, _arguments, P, generator) {
-		function adopt(value) {
-			return value instanceof P
-				? value
-				: new P(function (resolve) {
-						resolve(value);
-				  });
-		}
-		return new (P || (P = Promise))(function (resolve, reject) {
-			function fulfilled(value) {
-				try {
-					step(generator.next(value));
-				} catch (e) {
-					reject(e);
-				}
-			}
-			function rejected(value) {
-				try {
-					step(generator["throw"](value));
-				} catch (e) {
-					reject(e);
-				}
-			}
-			function step(result) {
-				result.done
-					? resolve(result.value)
-					: adopt(result.value).then(fulfilled, rejected);
-			}
-			step(
-				(generator = generator.apply(thisArg, _arguments || [])).next()
-			);
-		});
-	};
-var __importDefault =
-	(this && this.__importDefault) ||
-	function (mod) {
-		return mod && mod.__esModule ? mod : { default: mod };
-	};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -49,47 +21,47 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 const port = 3003;
 app.get("/", (req, res) => {
-	const pool = openDb();
-	pool.query("select * from task", (error, result) => {
-		if (error) {
-			res.status(500).json({ error: error.message });
-		}
-		res.status(200).json(result.rows);
-	});
+    const pool = openDb();
+    pool.query("select * from task", (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(result.rows);
+    });
 });
 app.post("/new", (req, res) => {
-	const pool = openDb();
-	pool.query(
-		"insert into task (description) values ($1) returning *",
-		[req.body.description],
-		(error, result) => {
-			if (error) {
-				res.status(500).json({ error: error.message });
-			}
-			res.status(200).json({ id: result.rows[0].id });
-		}
-	);
+    const pool = openDb();
+    pool.query("insert into task (description) values ($1) returning *", [req.body.description], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ id: result.rows[0].id });
+    });
 });
-app.delete("/delete/:id", (req, res) =>
-	__awaiter(void 0, void 0, void 0, function* () {
-		const pool = openDb();
-		const id = parseInt(req.params.id);
-		pool.query("delete from task where id = $1", [id], (error, result) => {
-			if (error) {
-				res.status(500).json({ error: error.message });
-			}
-			res.status(200).json({ id: id });
-		});
-	})
-);
+app.delete("/delete/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const pool = openDb();
+    const id = parseInt(req.params.id);
+    pool.query("delete from task where id = $1", [id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ id: id });
+    });
+}));
 const openDb = () => {
-	const pool = new pg_1.Pool({
-		user: "postgres",
-		host: "localhost",
-		database: "todo",
-		password: "sanisu",
-		port: 5432,
-	});
-	return pool;
+    const pool = new pg_1.Pool({
+        // user: "postgres",
+        // host: "localhost",
+        // database: "todo",
+        // password: "sanisu",
+        // port: 5432,
+        user: "root",
+        host: "dpg-cgkana0rddleudtqpqdg-a.oregon-postgres.render.com",
+        database: "todo_ax1o",
+        password: "2yvRrM208aQkLYRWUsJ2ux0VDauyw9Us",
+        port: 5432,
+        ssl: true,
+    });
+    return pool;
 };
 app.listen(port);
